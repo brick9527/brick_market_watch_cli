a3c47f0e487c1c06df5ee4e9ecee0c5e0316bf4c
brick9527 <brick9527@foxmail.com>
2025-11-22 22:44:41 +0800

    feat: 添加schedule定时询盘

---
+++ bin/schedule.js
@@ -0,0 +1,26 @@
+const nodeSchedule = require("node-schedule");
+const path = require("path");
+
+const { readFile } = require("../src/util/file");
+const { getTrickerPrice } = require("../src/controller/watch/watch");
+
+async function runSchedule() {
+  const scheduleConfigRawContent = readFile(
+    "schedule.json",
+    path.join(__dirname, "../")
+  );
+  const scheduleConfig = JSON.parse(scheduleConfigRawContent);
+
+  const interval = scheduleConfig.interval;
+
+  nodeSchedule.scheduleJob(interval, async () => {
+
+    const configRawContent = readFile("config.json", path.join(__dirname, "../"));
+    const config = JSON.parse(configRawContent);
+
+    await getTrickerPrice(config.symbols, true, true);
+  });
+}
+
+runSchedule();
+
